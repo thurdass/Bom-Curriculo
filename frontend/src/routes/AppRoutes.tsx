4e7f4a7a181be663b1d8abb45cd3efb6b5b91a22
Gerson Bruno Baptista <gersonbrunobaptista@gmail.com>
2026-07-25 16:30:16 -0300

    feat: altera css padrao por tailwind da tela my-curriculum (#186)

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -19,6 +19,7 @@ export function AppRoutes() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
+      
 
       <Route element={<ProtectedRoute />}>
         <Route path="/" element={<Home />} />
